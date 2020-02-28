da047e514d2412821578902086644b1702f3497b
Mukesh A <amukesh.mk@gmail.com>
2020-02-28 00:15:45 +0000

    added sentiment analyisis using GetOldTweets3 and TextBlob

---
+++ twitter_app.py
@@ -0,0 +1,18 @@
+import tweepy
+from twitter_config import *
+
+# Creating the authentication object
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+# Setting your access token and secret
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# Creating the API object while passing in auth information
+api = tweepy.API(auth)
+
+username = "nytimes"
+tweetCount = 1
+
+# Calling the user_timeline function with our parameters
+results = api.user_timeline(id=username, count=tweetCount)
+
+for tweet in results:
+   print(tweet.text)
