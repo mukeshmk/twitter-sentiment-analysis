52036e2b89013c0c92daee29a0a9b13b2b64022f
Mukesh A <amukesh.mk@gmail.com>
2020-03-05 18:12:29 +0000

    updated fetch data to handle exceptions and file saves

--- fetch_data.py
+++ fetch_data.py
@@ -1,23 +1,26 @@
-#https://pypi.org/project/GetOldTweets3/
-#https://github.com/Mottl/GetOldTweets3#egg=GetOldTweets3
-
-
+import os, re
 import GetOldTweets3 as got
 import pandas as pd
 
-start_date = "2019-11-08"
+dataset_foler = "datasets"
+file_name = ""
+file_ext = ".csv"
+
+start_date = "2020-02-06"
 end_date = "2020-02-08"
 language = "en"
 emoji = "unicode"
-maxtweets = 5
+maxtweets = 100000
 
-df = pd.DataFrame(columns=['count', 'date', 'username', 'to', 'replies', 'retweets' , 'text', 'mentions', 'hashtags', 'permalink'])
-df.set_index('count',inplace = True)
+# creating new DF everytime this program is run
+# to avoid repeat of data if the df is being appended
+df = pd.DataFrame(columns=['date', 'username', 'to', 'replies', 'retweets' , 'text', 'mentions', 'hashtags', 'permalink'])
 
 def receiveBuffer(tweets):
     global df
     for t in tweets:
-        data = [t.date.strftime("%Y-%m-%d %H:%M:%S"),
+        data = pd.Series([
+            t.date.strftime("%Y-%m-%d %H:%M:%S"),
             t.username,
             t.to,
             t.replies,
@@ -25,15 +28,33 @@ def receiveBuffer(tweets):
             t.text,
             t.mentions,
             t.hashtags,
-            t.permalink]
-        df.loc[0 if pd.isnull(df.index.max()) else df.index.max() + 1] = data
+            t.permalink], index=df.columns)
+        df = df.append(data, ignore_index=True)
 
-tweetCriteria = got.manager.TweetCriteria().setQuerySearch("elections")\
-                                            .setSince(start_date)\
-                                            .setUntil(end_date)\
-                                            .setMaxTweets(maxtweets)\
-                                            .setLang(language)\
-                                            .setEmoji(emoji)
-tweets = got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+query = input("Enter Search Query: ")
+try:
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch(query)\
+                                                .setSince(start_date)\
+                                                .setUntil(end_date)\
+                                                .setMaxTweets(maxtweets)\
+                                                .setLang(language)\
+                                                .setEmoji(emoji)
+    tweets = got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+except Exception as err:
+    pass
 
-df.to_csv('test.csv')
+# creating the dataset directory if it doesn't exist
+if not os.path.exists(dataset_foler):
+        os.makedirs(dataset_foler)
+
+# creating file name based on the query
+file_name = re.sub("[@#]","", query) + '_' + start_date + '_to_' + end_date + '_' + str(df.size)
+
+# to avoid duplicate file names in folder
+path = dataset_foler + '/%s%s' % (file_name, file_ext)
+uniq = 1
+while os.path.exists(path):
+  path = dataset_foler + '/%s_%d%s' % (file_name, uniq, file_ext)
+  uniq += 1
+
+df.to_csv(path, index=False)
