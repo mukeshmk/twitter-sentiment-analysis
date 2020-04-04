b56b7ff4325926ddff94c12742474edd0fd53686
Mukesh A <amukesh.mk@gmail.com>
2020-04-04 13:21:47 +0100

    reworked how the results are being displayed

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -8,6 +8,7 @@ from nltk.tokenize import word_tokenize
 from nltk import FreqDist, classify, NaiveBayesClassifier
 import GetOldTweets3 as got
 import pandas as pd
+import emoji
 
 import re, string, random
 
@@ -22,6 +23,8 @@ def remove_noise(tweet_tokens, stop_words = ()):
 
         # regex to removes the @twitter_handle (mentions)
         token = re.sub("(@[A-Za-z0-9_]+)","", token)
+
+        token = emoji.demojize(token)
 
         if tag.startswith("NN"):
             pos = 'n'
@@ -68,7 +71,7 @@ if __name__ == "__main__":
     all_pos_words = get_all_words(positive_cleaned_tokens_list)
 
     freq_dist_pos = FreqDist(all_pos_words)
-    print(freq_dist_pos.most_common(10))
+    # print(freq_dist_pos.most_common(10))
 
     positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
     negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
@@ -86,15 +89,22 @@ if __name__ == "__main__":
     train_data = dataset[:7000]
     test_data = dataset[7000:]
 
+    print('Training the Classifier!')
     classifier = NaiveBayesClassifier.train(train_data)
+    print("Accuracy is of Trained Classifier is:", classify.accuracy(classifier, test_data))
 
-    print("Accuracy is:", classify.accuracy(classifier, test_data))
-
-    print(classifier.show_most_informative_features(10))
+    # print(classifier.show_most_informative_features(10))
 
     df = pd.read_csv("datasets/ge2020_2020-02-06_to_2020-02-08_111726.csv")
 
+    print('Classifying the Data!')
+    pos_count = neg_count = 0
     for index, row in df.iterrows():
         tokenised_tweet = remove_noise(word_tokenize(row['text']))
-        print(row['text'], classifier.classify(dict([token, True] for token in tokenised_tweet)))
-        print()
+        if(classifier.classify(dict([token, True] for token in tokenised_tweet)) == 'Positive'):
+            pos_count += 1
+        else:
+            neg_count += 1
+
+    print('Positive Count ' + str(pos_count))
+    print('Negative Count ' + str(neg_count))
